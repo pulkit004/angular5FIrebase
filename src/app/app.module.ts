16e3d6d55cf2f369b8030ebb555f5c95a25b7812
Pulkit Verma <pulkitverma004@gmail.com>
2018-01-24 00:50:24 +0530

    Fina;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuthModule, AngularFireAuthProvider } from 'angularfire2/auth';
 import { AppRoutingModule } from './app-routing.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CoursesComponent } from './courses/courses.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFireDatabase } from 'angularfire2/database';
+
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  coursesObservable: Observable<any>;
+
+  constructor(private db: AngularFireDatabase) { }
+
+  ngOnInit() {
+    this.coursesObservable = this.getCourses('/courses');
+
+  }
+
+  getCourses(listPath): Observable<any[]> {
+    return this.db.list(listPath).valueChanges();
+  }
+
+}
